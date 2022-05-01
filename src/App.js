5fad78d39e89668a1f584869fee80c1f6db08e36
Abdullah <abououd4@gmail.com>
2022-05-01 19:36:04 +0300

    Add the navbar and set up intial naviagtion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Navbar></Navbar>
+        <div className="content">
+          <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
+            <Route path='/signup' element={<Signup></Signup>}></Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
