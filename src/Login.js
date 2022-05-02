b23f75512e822667a3411299a85332314bd56db6
Abdullah <abououd4@gmail.com>
2022-05-02 17:25:14 +0300

    login interface completed

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,31 @@
+import { Link } from "react-router-dom";
+
+const loginFunction = () => {
+    console.log('submit');
+}
+
 const Login = () => {
-    return ( 
-        <div>
-            
+    return (
+        <div className="sign">
+            <h2>Login</h2>
+            <form onSubmit={loginFunction}>
+                <label>Passport Number</label>
+                <input
+                    type="number"
+                    required
+                />
+                <label>Password</label>
+                <input
+                type="password"
+                required
+                />
+                <div className="button">
+                    <button>Login</button>
+                </div>
+            </form>
+            <p>Don't have an account? <Link to='/signup'>signup</Link></p>
         </div>
      );
 }
- 
+
 export default Login;
--- src/Signup.js
+++ src/Signup.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Signup = () => {
-    return ( 
-        <div>
-            
+    return (
+        <div className="sign">
+            <h2>Signup</h2>
+            <p>Have an account? <Link to='/login'>login</Link></p>
         </div>
      );
 }
- 
+
 export default Signup;
