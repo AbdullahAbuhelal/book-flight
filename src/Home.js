5ad1cf53e1dc929f1534edced5d3349f28989480
Abdullah <abououd4@gmail.com>
2022-05-04 02:25:00 +0300

    search flight interface added

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,41 @@
 const Home = () => {
-    return ( 
-        <div>
-            <h2>Home page</h2>
+
+    const cities = [
+        {name: 'Riyadh', acronym: 'RUH'},
+        {name: 'Jeddah', acronym: 'JED'},
+        {name: 'Dammam', acronym: 'DMM'},
+    ];
+
+    return (
+        <div className="home">
+            <h2>Book a flight</h2>
+            <form>
+                <div>
+                    <h3>Departure</h3>
+                        <label>City</label>
+                        <select>
+                            {cities.map((city) => (
+                                <option key='${city.name}'>{city.name} ({city.acronym})</option>
+                            ))}
+                        </select>
+                        <label>Date</label>
+                        <input type="date" />
+                </div>
+                <div>
+                    <h3>Destination</h3>
+                        <label>City</label>
+                        <select>
+                            {cities.map((city) => (
+                                <option key='${city.name}'>{city.name} ({city.acronym})</option>
+                            ))}
+                        </select>
+                        <label>Date</label>
+                        <input type="date" />
+                </div>
+                <button>Search</button>
+            </form>
         </div>
      );
 }
- 
+
 export default Home;
